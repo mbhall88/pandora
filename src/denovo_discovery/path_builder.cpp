4a7a8e6588c0a288e292168b39e713b815f9c193
Michael Hall <michael@mbh.sh>
2019-09-20 15:15:18 +0100

    create PathBuilder

---
+++ src/denovo_discovery/path_builder.cpp
@@ -0,0 +1,5 @@
+//
+// Created by michael on 20/9/19.
+//
+
+#include "path_builder.h"
---
+++ src/denovo_discovery/path_builder.h
@@ -0,0 +1,11 @@
+//
+// Created by michael on 20/9/19.
+//
+
+#ifndef PANDORA_PATH_BUILDER_H
+#define PANDORA_PATH_BUILDER_H
+
+class path_builder {
+};
+
+#endif // PANDORA_PATH_BUILDER_H
